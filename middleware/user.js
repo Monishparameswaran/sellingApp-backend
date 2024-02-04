17a5ab4524be58b73ff9d3949bfb32035231d3a3
Monishparameswaran <monishparameshwaran@gmail.com>
2024-02-04 12:05:56 +0530

    User authentication is shifted to JWT

--- middleware/user.js
+++ middleware/user.js
@@ -1,16 +1,21 @@
 const { User }=require("../db");
+const jwt=require("jsonwebtoken");
+const JWT_SECRET="monish";
+
 async function userMiddleware(req, res, next) {
-    const username=req.headers.username;
-    const password=req.headers.password;
-    const resp=await User.find({
-        username,
-        password
-    });
-    if(resp){
+    // const username=req.headers.username;
+    // const password=req.headers.password;
+    // const resp=await User.find({
+    //     username,
+    //     password
+    // });
+    const token=req.headers.authorization;
+    const response=jwt.verify(token,JWT_SECRET);
+    if(response.username){
         next();
     }
     else{
-        res.status(400).json({msg: " Username or Password of User is wrong"});
+        res.status(400).json({msg: " Invalid token for user"});
     }
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
