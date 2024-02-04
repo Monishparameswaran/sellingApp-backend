f518943073138eb67257bcf16682b31ccec170a6
Monishparameswaran <monishparameshwaran@gmail.com>
2024-02-04 12:04:51 +0530

    updated admin middleware to authenticate user based on JWT token instead of username and passord each time

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,17 +1,26 @@
 // Middleware for handling auth
 const { Admin }=require("../db");
+const jwt=require("jsonwebtoken");
 async function adminMiddleware(req, res, next) {
-    const user=req.headers.username;
-    const password=req.headers.password;
-    const resp=await Admin.find({
-        user,
-        password
-    });
-    if(resp){
+    
+    // using username and password
+    // const user=req.headers.username;
+    // const password=req.headers.password;
+    // const resp=await Admin.find({
+    //     user,
+    //     password
+    // });
+
+    // use jwt tokens 
+    const JWT_SECRET="monish";
+    const data=req.headers.authorization;
+    const resp=jwt.verify(data,JWT_SECRET);
+
+    if(resp.username){
         next();
     }
     else{
-        res.status(400).json({msg: " Username or Password of Admin is wrong"});
+        res.status(400).json({msg: " Invalid Token !"});
     }
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
